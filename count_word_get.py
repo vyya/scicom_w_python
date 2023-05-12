219280fe84f80d38073f7bc2e7b88e4a9984de06
neuro <yamsh@yandex.ru>
2023-05-12 15:49:36 +0600

    create list of tuples w/.append and sort it out in reverse order

--- count_word_get.py
+++ count_word_get.py
@@ -7,10 +7,15 @@ for line in handle:
         counts[word] = counts.get(word, 0) + 1
 print('Number of words fox: ', counts.get('fox', 0))
 print('Number of every word as dictionary: ', counts)
-bigcount = None
+'''bigcount = None
 bigword = None
 for word, count in counts.items():
     if bigcount is None or count > bigcount:
         bigword = word 
         bigcount = count
-print(bigword, bigcount)
+print(bigword, bigcount)'''
+tmp = list()
+for word, count in counts.items():
+    tmp.append((count, word))
+tmp = sorted(tmp, reverse=True)
+print(f"Here is the descending word frequency in a given text: {tmp}")
