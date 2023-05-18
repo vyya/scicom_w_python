2cc8ba9c6794a4413ce54ec11215ea50ecd5202d
neuro <yamsh@yandex.ru>
2023-05-18 18:34:53 +0600

    SSL context added to ignore SSL errors

--- bs4_parser.py
+++ bs4_parser.py
@@ -1,5 +1,12 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
 
 url = input('Enter - ')
 html = urllib.request.urlopen(url).read()
